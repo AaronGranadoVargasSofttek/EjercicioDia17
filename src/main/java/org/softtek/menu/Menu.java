8e8fa04ae151f57e5ed1b010943ebcb33e333abc
SOFTTEKPROJECTS\alfredo.montaner <alfredo.montaner@softtek.com>
2026-03-18 16:16:57 +0100

    Funciones 1 y 2 de Menú y creacion de List<Empleado> en main

--- src/main/java/org/softtek/menu/Menu.java
+++ src/main/java/org/softtek/menu/Menu.java
@@ -1,27 +1,42 @@
 package org.softtek.menu;
 
+import org.softtek.model.Empleado;
+
+import java.util.List;
+
 public class Menu {
 
-    public static String iniciarElecciones() {
+    public static String iniciarElecciones()
+    {
         return ("1- Imprimir Directorio" +
                 "2- Buscar Usuario " +
                 "3- Guardar Directorio " +
                 "4- Leer Directorio");
     }
 
-    public static void imprimirDirectorio() {
+    public static void imprimirDirectorio(List<Empleado> empleados)
+    {
+        for (Empleado e : empleados)
+        {
+            System.out.println(e.nombre()+ " "+e.apellido()+"\n---\n");
+        }
+    }
+
+    public static void buscarUsuario(List<Empleado> empleados, String nombreBuscado)
+    {
+        Empleado empleado = empleados.stream()
+                .filter(e -> e.getNombre().equalsIgnoreCase(nombreBuscado))
+                .findFirst();
+        System.out.println(empleado.nombre()+" "+ empleado.apellido());
+    }
+
+    public static void guardarDirectorio()
+    {
 
     }
 
-    public static void buscarUsuario() {
-
-    }
-
-    public static void guardarDirectorio() {
-
-    }
-
-    public static void leerDirectorio() {
+    public static void leerDirectorio()
+    {
 
     }
 
