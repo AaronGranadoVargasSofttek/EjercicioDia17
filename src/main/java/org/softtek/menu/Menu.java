6d648061c21c423b1a866a8b97fe364409a6888a
SOFTTEKPROJECTS\alejandro.fernandezi <alejandro.fernandezi@softtek.com>
2026-03-18 16:25:26 +0100

    Guardar Directorio

--- src/main/java/org/softtek/menu/Menu.java
+++ src/main/java/org/softtek/menu/Menu.java
@@ -1,5 +1,13 @@
 package org.softtek.menu;
 
+import org.softtek.model.Empleado;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import static org.softtek.model.Utilidades.getEmpleados;
+
 public class Menu {
 
     public static String iniciarElecciones() {
@@ -19,6 +27,19 @@ public class Menu {
 
     public static void guardarDirectorio() {
 
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            PrintWriter writejson = new PrintWriter("directorio.json");
+
+            String json = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(getEmpleados());
+
+            writejson.write(json);
+            writejson.close();
+            System.out.println("Guardado correctamente");
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
     }
 
     public static void leerDirectorio() {
